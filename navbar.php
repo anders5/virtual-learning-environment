a3814ea1b90a2fa539c993bb0f7ba67eaf3e71af
Andrea-V <valentiandrea@rocketmail.com>
2015-03-13 10:59:41 +0000

    other minor changes

--- navbar.php
+++ navbar.php
@@ -2,6 +2,7 @@
 	<ul>
 		<li><a href="account.php">Account page</a>
 		<li><a href="group.php">Group page</a>
-		<li><a href="#">Log out</a>
+		<li><a href="forum.php">Forum</a>
+		<li><a href="logout.php">Log out</a>
 	</ul>
 </nav>
