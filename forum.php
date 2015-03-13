ff7549d31645ea3aade205e6e470da23d442d2e3
Andrea-V <valentiandrea@rocketmail.com>
2015-03-13 10:59:05 +0000

    Added forum.php and thread.php
    these pages implements the forum requirement

---
+++ forum.php
@@ -0,0 +1,93 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Forum</title>
+	<?php 
+		require_once('init.php');
+		$connection=db_connect();
+		$uid	=$_SESSION['uid'];
+		
+		//new thread created, store it in the db
+		if(isset($_POST['newpost'])){
+			$title	=mysqli_real_escape_string($connection,$_POST['title']);
+			$content=mysqli_real_escape_string($connection,$_POST['content']);
+			
+			$query="INSERT INTO threads VALUES(NULL,$uid,'$title','$content',CURRENT_TIMESTAMP)";
+			$result=mysqli_query($connection,$query);
+			if(!$result)
+				echo "<i>ERROR: Couldn't create a new thread.</i>";
+			else
+				echo "<i>Thread successfully created.</i>";
+		}
+	?>
+</head>
+<body>
+	<header>
+	<h1>Public Forum</h1>
+	<?php require_once('navbar.php'); ?>
+	</header>
+	
+	<main>
+		<article>
+			<h2>Latest discussions:</h2>
+			<?php
+				$query="SELECT threads.id,title,username,creation FROM threads JOIN students ON threads.fk_author=students.id ORDER BY creation DESC";
+				$result=mysqli_query($connection,$query) or die('Error in mySQL query'.mysql_error());
+				
+				if(mysqli_num_rows($result)==0)
+						echo "<i>There are no discussions yet.</i><br />";
+				else{
+					?>
+					<section>
+						<table border=1>
+						<?php
+							while($row=mysqli_fetch_array($result))
+								print_forum_post($row['username'],$row['creation'],"<a href='thread.php?id=$row[id]'><b>$row[title]</b></a>");
+						?>
+						</table>
+					</section>
+					<section>
+						<form action="#" method=post  >
+						<legend><h3>Post a new thread here:</h3></legend>
+						<fieldset>
+							<input type=text name=title size=50 maxlenght=50 placeholder='Write the title here...'/><br />
+							<textarea name=content rows=5 cols=50 placeholder='Write here...'></textarea><br />
+							<input type=hidden name=newpost value=1>
+							<input type=submit value='Post'>
+						</fieldset>
+						</form>
+					</section>
+				<?php
+				}
+				?>
+		</article>
+		<aside>
+			<form action="forum_search_result.php" method=get  >
+			<legend><h3>Search a thread:</h3></legend>
+				<fieldset>
+					<i>Enter the partial title below.</i><br />
+					<input type=text name=title size=30 maxlenght=50 /><br />
+					<input type="checkbox" name="user_src" />
+					Search only threads by:
+					 <select name="userlist">
+						<?php
+							$query="SELECT username FROM students ORDER BY username";
+							$result=mysqli_query($connection,$query) or die('Error in mySQL query'.mysql_error());
+							while($row=mysqli_fetch_array($result))
+								echo "<option value='$row[username]'>$row[username]</option>";
+						?>
+					</select>
+					<br />
+					<input type=hidden name=newpost value=1>
+					<input type=submit value='Search'>
+				</fieldset>
+			</form>
+		</aside>
+	</main>
+	
+	<footer>
+	</footer>
+    </body>
+</html>
---
+++ thread.php
@@ -0,0 +1,86 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />  
+	<meta charset="utf-8">
+	<title>Forum</title>
+	<?php 
+		require_once('init.php');
+		$connection=db_connect();
+		$uid=6;//$_SESSION['uid'];
+		$tid=(int)(isset($_POST['newrep']))?($_POST['tid']):($_GET['id']);
+		
+		//new thread created, store it in the db
+		if(isset($_POST['newrep'])){
+			$content=mysqli_real_escape_string($connection,$_POST['content']);
+			
+			$query="INSERT INTO replies VALUES(NULL,$tid,$uid,'$content',CURRENT_TIMESTAMP)";
+			$result=mysqli_query($connection,$query);
+			if(!$result)
+				echo "<i>ERROR: Couldn't store the reply in the db.</i>";
+			else
+				echo "<i>Reply successfully stored.</i>";
+		}
+		
+	?>
+</head>
+<body>
+	<header>
+	<?php require_once('navbar.php'); ?>
+	</header>
+	
+	<main>
+		<?php
+			$query="SELECT title,content,username,creation FROM threads JOIN students ON threads.fk_author=students.id WHERE threads.id=$tid"; 
+			$result=mysqli_query($connection,$query) or die('Error in mySQL query'.mysqli_error($connection));
+			$row=mysqli_fetch_array($result);
+		?>
+		<article>
+			<h2><?php echo $row['title'] ?></h2>
+		
+			<section>
+				<table border=1>
+					<?php print_forum_post($row['username'],$row['creation'],$row['content']); ?>
+				</table>
+			</section>
+			<br>
+			<section>
+				<i>Replies:</i><br />
+				<?php
+					$query="SELECT content,creation,username FROM replies JOIN students ON replies.fk_author=students.id WHERE fk_thread=$tid ORDER BY creation ASC"; 
+					$result=mysqli_query($connection,$query) or die('Error in mySQL query'.mysqli_error($connection));
+					if(mysqli_num_rows($result)==0)
+						echo "<i>There are no replies to this thread yet.</i><br />";
+					else{
+						echo "<table border=1>";
+	
+						while($row=mysqli_fetch_array($result))
+							print_forum_post($row['username'],$row['creation'],$row['content']);
+						
+						echo "</table>";
+					}
+				?>
+			</section>
+			<section>
+				<form action="#" method=post  >
+				<legend><h3>Post a new reply here:</h3></legend>
+				<fieldset>
+					<textarea name=content rows=5 cols=50 placeholder='Write your reply here...'></textarea><br />
+					<input type=hidden name=newrep value=1>
+					<input type=hidden name=tid value=<?php echo $tid?>>
+					<input type=submit value='Post'>
+				</fieldset>
+				</form>
+			</section>
+		</article>
+		
+		<aside>
+			Click <a href="forum.php">here</a> to go back to the main forum.
+		</aside>
+	</main>
+	
+	<footer>
+	</footer>
+    </body>
+</html>
