9bb17589776e259fd16dd209694ccf06834cdf23
Andy Whitford <zcabg80@live.ucl.ac.uk>
2015-03-20 17:00:15 +0000

    Updated database password

--- base.php
+++ base.php
@@ -5,7 +5,7 @@ require_once("init.php");
   
     $host = "eu-cdbr-azure-north-b.cloudapp.net";
     $user = "b71a879e88f712";
-    $pwd = "Databases1";
+    $pwd = "6c9071b0";
     $db = "vledatabase";
     // Connect to database.
     $connection = mysqli_connect($host,$user,$pwd,$db) or die("MySQL Error: " . mysql_error());
