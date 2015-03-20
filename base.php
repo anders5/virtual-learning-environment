d54e4db58ad75baec17ce2345d301f816465c39c
Andy Whitford <zcabg80@live.ucl.ac.uk>
2015-03-20 16:54:14 +0000

    Changed database connection info

--- base.php
+++ base.php
@@ -3,10 +3,10 @@
 require_once("init.php");
 
   
-    $host = "localhost";
-    $user = "root";
-    $pwd = "";
-    $db = "virtual_learning_environment";
+    $host = "eu-cdbr-azure-north-b.cloudapp.net";
+    $user = "b71a879e88f712";
+    $pwd = "Databases1";
+    $db = "vledatabase";
     // Connect to database.
     $connection = mysqli_connect($host,$user,$pwd,$db) or die("MySQL Error: " . mysql_error());
     
